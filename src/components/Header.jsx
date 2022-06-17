ef624a49b4cb3f7f6a509d48fcc5b02dcf46a61b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-16 22:33:02 -0300

    feat: header criado

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useContext } from "react";
+import { MyContext } from "../context/Provider";
+import { User } from "./User";
+import { firebase, auth } from '../services/firebase';
+
+export function Header() {
+  const { user, setUser } = useContext(MyContext);
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider)
+      .then(result => {
+        if (result.user) {
+          const { displayName: name, photoURL: avatar, uid: id } = result.user;
+          setUser(name, avatar, id);
+        } else {
+          window.alert('Impossível realizar login. Tente novamente!');
+        }
+      });
+  }
+
+  return (
+    <header>
+      <h1>Cloneddit</h1>
+      <form>
+        <input type="text" placeholder="Buscar no Cloneddit" />
+      </form>
+      {(!user) ? (
+        <button onClick={ signInWithGoogle }>
+          Login
+        </button>
+      ) : <User user={user} />}
+    </header>
+  );
+}
