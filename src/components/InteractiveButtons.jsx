39cd2480f8f3da00e885e07c975e825f85de95cb
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-20 12:52:52 -0300

    feat: adicionada função de remover post

---
+++ src/components/InteractiveButtons.jsx
@@ -0,0 +1,65 @@
+import React, { useContext } from "react";
+
+import { MyContext } from "../context/Provider";
+import { database } from "../services/firebase";
+import commentsImg from '../assets/images/comment.svg';
+import deleteImg from '../assets/images/delete.svg';
+
+export function InteractiveButtons({ post, index, enableComments }) {
+  const { user } = useContext(MyContext);
+
+  const findMyLike = (post) => {
+    return post.likes.find((like) => like.authorId === user.id);
+  };
+
+  const likeThePost = async (post) => {
+    if (findMyLike(post)) {
+      await database.ref(`allPosts/${post.author.id}/posts/${post.postId}/likes/${findMyLike(post).likeId}`)
+        .remove();
+    } else {
+      await database.ref(`allPosts/${post.author.id}/posts/${post.postId}/likes`)
+        .push(user.id);
+    }
+  };
+
+  const deletePost = async () => {
+    await database.ref(`allPosts/${post.author.id}/posts/${post.postId}`).remove();
+  }
+
+  return (
+    <div className="interactive-buttons">
+      <button
+        type="button"
+        onClick={ () => enableComments(index) }
+        aria-label="Abrir aba de comentários"
+        title="Comentários"
+        disabled={ !user }
+      >
+        <span>{post.comments.length}</span>
+        <img src={ commentsImg } alt="Comentário"/>
+      </button>
+      <button
+        type="button"
+        className={(user && findMyLike(post)) && 'liked'}
+        onClick={ () => likeThePost(post) }
+        aria-label="Marcar como gostei"
+        title="Like"
+        disabled={ !user }
+      >
+        <span>{post.likes.length}</span>
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </button>
+      {(user && user.id === post.author.id) && (
+        <button
+          type="button"
+          onClick={ deletePost }
+          aria-label="Remover post"
+        >
+          <img src={ deleteImg } alt="Remover" title="Remover"/>
+        </button>
+      )}
+    </div>
+  );
+}
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,15 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { User } from "./User";
-import { useState, useContext } from "react";
-import { MyContext } from "../context/Provider";
 import { Comments } from "./Comments";
 
-import commentsImg from '../assets/images/comment.svg';
 import '../styles/postsList.css';
-import { database } from "../services/firebase";
+import { InteractiveButtons } from "./InteractiveButtons";
 
 export function PostsLists({ allPosts }) {
-  const { user } = useContext(MyContext);
   const [showComment, setShowComment] = useState(false);
   const [commentIndex, setCommentIndex] = useState(-1);
 
@@ -32,7 +28,7 @@ export function PostsLists({ allPosts }) {
       return array;
     } 
     return [];
-  }
+  };
 
   const getLikes = (likesObject) => {
     if (likesObject) {
@@ -42,7 +38,7 @@ export function PostsLists({ allPosts }) {
       }));
     }
     return [];
-  }
+  };
 
   const putAllPostsInsideASingleArray = () => {
     if(allPosts[0] && !allPosts[0].posts) return [];
@@ -51,7 +47,7 @@ export function PostsLists({ allPosts }) {
       const authorInfo = Object.values(getPosts[0][1]);
       const getPostsInfo = Object.entries(getPosts[1][1] ?? {});
       const newArrayPost = getPostsInfo.map((postInfo) => ({
-        user: authorInfo[0],
+        author: authorInfo[0],
         postId: postInfo[0],
         ...postInfo[1],
         comments: getComments(postInfo[1].comments),
@@ -67,26 +63,12 @@ export function PostsLists({ allPosts }) {
   const formatDate = (date) => {
     const newDate = date.split(' ');
     return (`${newDate[1]} - ${newDate[0]}`)
-  }
+  };
 
   const enableComments = (index) => {
     setShowComment(true);
     setCommentIndex(index);
-  }
-
-  const findMyLike = (post) => {
-    return post.likes.find((like) => like.authorId === user.id);
-  }
-
-  const likeThePost = async (post) => {
-    if (findMyLike(post)) {
-      await database.ref(`allPosts/${post.user.id}/posts/${post.postId}/likes/${findMyLike(post).likeId}`)
-        .remove();
-    } else {
-      await database.ref(`allPosts/${post.user.id}/posts/${post.postId}/likes`)
-        .push(user.id);
-    }
-  }
+  };
 
   console.log(putAllPostsInsideASingleArray());
 
@@ -98,30 +80,12 @@ export function PostsLists({ allPosts }) {
           <h2 className="title">{post.title}</h2>
           <p className="content">{post.content}</p>
           <footer>
-            <User user={ post.user } />
-            <div className="interactive-buttons">
-              <button
-                type="button"
-                onClick={ () => enableComments(index) }
-                aria-label="Abrir aba de comentários"
-                disabled={ !user }
-              >
-                <span>{post.comments.length}</span>
-                <img src={ commentsImg } alt="Comentário" title="Comentários"/>
-              </button>
-              <button
-                type="button"
-                className={(findMyLike(post)) && 'liked'}
-                onClick={ () => likeThePost(post) }
-                aria-label="Marcar como gostei"
-                disabled={ !user }
-              >
-                <span>{post.likes.length}</span>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z" stroke="#737380" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-              </button>
-            </div>
+            <User user={ post.author } />
+            <InteractiveButtons
+              post={ post }
+              index={ index }
+              enableComments={ enableComments }
+            />
           </footer>
           {(showComment && commentIndex === index) && (
             <Comments
