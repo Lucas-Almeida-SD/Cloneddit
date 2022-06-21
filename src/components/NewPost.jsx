96c8febb4ce4bdcb0364a4f22a157ef16834295b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-21 17:39:31 -0300

    feat: criada função de adicionar uma imagem a um novo post

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -4,12 +4,33 @@ import { useContext } from "react";
 import { MyContext } from "../context/Provider";
 import { database } from "../services/firebase";
 
+import imageImg from '../assets/images/image-icon.png';
+
 import '../styles/newPost.css';
 
 export function NewPost() {
   const { user } = useContext(MyContext);
   const [inputValue, setInputValue] = useState('');
   const [textareaValue, setTextareaValue] = useState('');
+  const [fileValue, setFileValue] = useState('');
+
+  const selectFile = () => {
+    document.getElementById('select-file').click();
+  }
+
+  const handleChangeFile = ({ target }) => {
+    let reader = new FileReader();
+    reader.onload = () => {
+      setFileValue(reader.result);
+    }
+    reader.readAsDataURL(target.files[0])
+  }
+
+  const removeImage = () => {
+    if (window.confirm('Deseja remover a imagem?')) {
+      setFileValue('');
+    }
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -17,6 +38,7 @@ export function NewPost() {
       author: user,
       title: inputValue,
       content:textareaValue,
+      image: fileValue,
       postedAt: new Date().toLocaleString('pt-BR', { timeZone: "America/Sao_Paulo" }),
     });
 
@@ -34,12 +56,28 @@ export function NewPost() {
           onChange={ ({ target }) => setInputValue(target.value) }
           disabled={ !user }
         />
-        <textarea
-          placeholder="Insira o seu texto"
-          value={ textareaValue }
-          onChange={ ({ target }) => setTextareaValue(target.value) }
+        <div className="textarea-div">
+          <textarea
+            placeholder="Insira o seu texto"
+            value={ textareaValue }
+            onChange={ ({ target }) => setTextareaValue(target.value) }
+            disabled={ !user }
+          />
+          <div>
+            <img src={ imageImg } alt="Imagem" title="Imagem" onClick={ selectFile }/>
+          </div>
+        </div>
+        <input
+          type="file"
+          id="select-file"
+          accept="image/*"
+          onChange={ handleChangeFile }
+          style={ { display: 'none' } }
           disabled={ !user }
         />
+        <div className="image-preview" style={ { display: (!fileValue) && 'none' } }>
+          <img src={ fileValue } alt="Pré-visualização da imagem" onClick={ removeImage }/>
+        </div>
         <footer className={(user) && 'footer-logged'}>
           {(!user) && <span>Faça login para interagir!</span>}
           <button
