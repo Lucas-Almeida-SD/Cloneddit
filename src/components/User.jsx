a718102763f8a5b5dd4420ecc881fa61b08362ac
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-26 23:46:05 -0300

    chore: adicionando o atributo "referrerPolicy" na imagem do User

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,7 +5,7 @@ import '../styles/user.css';
 export function User({ user }) {
   return (
     <div id="user">
-      <img src={ user.avatar } alt={ user.name } />
+      <img src={ user.avatar } alt={ user.name } referrerPolicy="no-referrer" />
       <span>{user.name}</span>
     </div>
   );
